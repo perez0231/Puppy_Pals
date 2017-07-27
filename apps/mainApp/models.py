bb6051c6c129e6b0d2bfe0d84f3ea24c2f8e7d8a
Daniel Perez <perez0231@yahoo.com>
2017-07-27 13:45:33 -0400

    updated models

--- apps/mainApp/models.py
+++ apps/mainApp/models.py
@@ -11,35 +11,33 @@ class Dogs(models.Model):
     size = models.CharField(max_length=45)
     breed = models.CharField(max_length=45)
     zipcode= models.IntegerField(max_length=5)
-
-    picture = models.CharField()
-
+    picture = models.CharField(max_length=1000)
     created_at = models.DateTimeField(auto_now_add=True)
-	updated_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
 class Requests(models.Model):
-    user = models.ForeignKey(Users, related_name='dog_Owner')
-    inviter = models.ForeignKey(Users, related_name='dog_Inviter')
+    user = models.ForeignKey(Users, related_name='request_dog_Owner')
+    inviter = models.ForeignKey(Users, related_name='request_dog_Inviter')
     created_at = models.DateTimeField(auto_now_add=True)
-	updated_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
 
 class Pals(models.Model):
     user = models.ForeignKey(Users, related_name='owner_Pals')
     pals = models.ForeignKey(Users, related_name='user_Pals')
     created_at = models.DateTimeField(auto_now_add=True)
-	updated_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
-class Messages
+class Messages(models.Model):
     user = models.ForeignKey(Users, related_name='user_Message')
     pal = models.ForeignKey(Pals, related_name='pal_Message')
-    message = models.CharField()
+    message = models.CharField(max_length=1000)
     created_at = models.DateTimeField(auto_now_add=True)
-	updated_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
-class Playdates
+class Playdates(models.Model):
     user = models.ForeignKey(Users, related_name='owner_Playdate')
     pal = models.ForeignKey(Users, related_name='pal_Playdate')
-    location = models.CharField()
+    location = models.CharField(max_length=1000)
     created_at = models.DateTimeField(auto_now_add=True)
-	updated_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now=True)
